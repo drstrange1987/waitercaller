d2bbe0c51a5487a91f6a69bb5e91f6da9dec3f81
drstrange1987 <mohamedmahdikouki@gmail.com>
2016-05-31 13:23:09 +0100

    lkjlj

--- dbhelper.py
+++ dbhelper.py
@@ -36,6 +36,7 @@ class DBHelper:
             self.db.requests.insert({"owner": table['owner'],
                 "table_number": table['number'],
                 "table_id": table_id, "time": time})
+            return True
         except pymongo.errors.DuplicateKeyError:
             return False
 
